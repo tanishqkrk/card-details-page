8a300595a48865b736e2c1429500ff2bf461510f
tanishqkrk <tanishqkrk@gmail.com>
2022-08-25 02:30:42 +0530

    new commit

---
+++ script-complete.js
@@ -0,0 +1,5 @@
+const completeButton = document.querySelector('.container__complete--button-button');
+
+completeButton.addEventListener('click', e => {
+    window.location.href = "./index.html";
+})
